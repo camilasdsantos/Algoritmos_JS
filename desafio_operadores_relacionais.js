d6d18a76cbcef9c9180f874bf006d6672ed85fca
camilasdsantos <99107296+camilasdsantos@users.noreply.github.com>
2022-10-04 19:50:38 -0300

    Add files via upload

---
+++ desafio_operadores_relacionais.js
@@ -0,0 +1,22 @@
+
+
+let a=1;
+let b=2;
+// encontre 6 operadores relacionais que o valor é true
+{console.log("operadores relacionais saída true");
+   console.log(b>a) ;
+   console.log(a<b);
+   console.log(a<=1);
+   console.log( b!=a);
+   console.log(b==2);
+   console.log(a +a ===b);
+}
+// encontre 6 operadores relacionais que o valor é False
+
+{console.log("operadores relacionais saída false");
+    console.log(b==a);
+    console.log(b===a);
+    console.log(b<a);
+    console.log(a>b);
+    console.log(b>=10)
+    console.log( b - a !==a);}
