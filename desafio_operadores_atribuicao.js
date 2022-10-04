4433166a0dca89076f7d27b220ea1da42e94c834
camilasdsantos <99107296+camilasdsantos@users.noreply.github.com>
2022-10-04 20:51:05 -0300

    Add files via upload

---
+++ desafio_operadores_atribuicao.js
@@ -0,0 +1,24 @@
+
+/*Desafio:
+ imprima uma lista de nome de aprovados usando "console.log()" apenas uma unica vez;
+ Mais operadores de atribuição aritméticos 
+ lembrando que:
+\n significa new line (nova linha)
+ o \n é um caracter especial que não aparece no console. Para ele aparecer:
+ boa como gambiarra uma bara a mais (igual ao $ a mais no automation anywhere, para o caracter nao ser lido como especial)
+t da um tab */
+let texto= "Lista de Aprovados";
+texto += "\n\n===================\n\n";
+texto += "1. Ana Carla Tavares Lima\n";
+texto += "2. Ana Paula da Silva\n";
+texto += "3. Breno Lucas Sousa\n";
+texto += "4. Cleivison Santos\n";
+texto += "5. Daniele Carvalho Leite\n";
+texto += "6. Euvira Medeiros\n";
+texto += "7. Francine Antonia de Paula\n";
+texto += "8. Glauber Marcos Travassos\n";
+texto += "9. Heloisa Siqueira\n";
+texto += "10.Ilê Avellar Puelles\n";
+texto += "11.Rebeca França\n";
+texto += "12.Zuleide Amaral";
+console.log(texto);
