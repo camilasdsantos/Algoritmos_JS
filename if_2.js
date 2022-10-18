103263b3078c5490783f573c52bb4f4b98d50dc2
camilasdsantos <99107296+camilasdsantos@users.noreply.github.com>
2022-10-18 19:53:39 -0300

    Add files via upload

---
+++ if_2.js
@@ -0,0 +1,11 @@
+const nota = 8;
+const bomComportamento = false;
+if (nota >= 7 && bomComportamento === true)
+    console.log("Parabéns você foi aprovado");
+    //shif alt F formatar código
+
+ if(nota < 7  || !bomComportamento)
+
+    console.log(" você NÃO foi aprovado");
+
+console.log("fim")
