0f8d5c21aa17a4e406a47fdde76bec4639d01980
camilasdsantos <99107296+camilasdsantos@users.noreply.github.com>
2022-10-18 15:45:44 -0300

    Add files via upload

---
+++ operador_ternario.js
@@ -0,0 +1,9 @@
+// operador ternario ou também conhecido como atribuição condicional
+// é composto de 3 partes:
+// a expressão;
+// a afirmaçao
+// a negação
+let hora = 14;
+let saudacao = hora <= 11 ? "Bom dia!" : "Boa Tarde!"  ;
+
+console.log(saudacao);
---
+++ operador_ternario2.js
@@ -0,0 +1,13 @@
+//colocando um operador ternario dentro de outro
+
+let hora = 23;
+let saudacao = hora <= 11 ? "Bom dia!" : hora <= 17 ? "Boa tarde!" : "Boa noite!"  ;
+
+console.log(saudacao);
+
+ hora = 07;
+     saudacao = hora <= 11 ? "Bom dia!" 
+    : hora <= 17 ? "Boa tarde!"
+    : "Boa noite!"  ;
+
+console.log(saudacao);
