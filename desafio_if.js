db0ce3f910261d53a1d9be0a082c40eee46d5a05
camilasdsantos <99107296+camilasdsantos@users.noreply.github.com>
2022-10-31 16:41:42 -0300

    Add files via upload

---
+++ desafio_if.js
@@ -0,0 +1,25 @@
+var nota = 4.3;
+
+
+if(nota >=9 || nota <=10)
+{
+    console.log("A");
+    console.log("Excelente")
+}
+if(nota >=7 || nota <=8.9){
+    console.log("B");
+    console.log("Parabéns")
+}
+if(nota >=5 || nota <=6.9){
+    console.log("C");
+    console.log("Razoavel");
+}
+if(nota >=4.5 || nota <=4.9 ){
+    console.log("D");
+    console.log("Precisa Melhorar");
+}
+if(nota <=4.5 ){
+    console.log("D");
+    console.log("Precisa Melhorar");
+}
+console.log("fim!")
