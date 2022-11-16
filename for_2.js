70165f6f93750cfd153616794f68e420ed64ec6f
camilasdsantos <99107296+camilasdsantos@users.noreply.github.com>
2022-11-16 11:17:56 -0300

    Add files via upload

---
+++ for_2.js
@@ -0,0 +1,16 @@
+/*laço infinito no for
+for (;;) {
+
+}
+console.log("fim")
+
+ou for(; true; ){
+
+} */
+
+let  controle =1;
+for (;controle <=10; controle ++){
+    console.log(controle);
+
+}
+console.log("fim");
