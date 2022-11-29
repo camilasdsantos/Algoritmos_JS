a9c9cbbd86842272cf62e0e71d72b9b4b1ac4462
camilasdsantos <99107296+camilasdsantos@users.noreply.github.com>
2022-11-29 00:14:30 -0300

    Add files via upload

---
+++ switch.js
@@ -0,0 +1,36 @@
+let nota =0.4;
+switch(Math.ceil(nota)) { /* ceil arredonda o valor para o maior numero interio mais próximo do valor*/
+default: 
+    console.log("nota invalida");
+    break;
+case 10:
+    console.log ("Excelente");
+    break;
+case 9:
+     console.log ("Parabéns");
+     break;
+case 8:
+    console.log ("Parabéns");
+    break;
+case 7:
+    console.log ("Bom");
+    break;
+case 6:
+    console.log ("OK");
+    break;
+case 5: 
+    console.log ("Na média, você passou!");
+    break;
+case 4:
+console.log ("reprovado");
+break;
+case 3:
+    console.log ("reprovado");
+    break;
+case 2:
+    console.log ("reprovado");
+    break;
+case 1:
+    console.log("reprovado");
+
+}
