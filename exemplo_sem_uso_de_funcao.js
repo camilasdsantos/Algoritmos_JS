49ad54a58acfa93e10e216355ae1a56112643bcd
camilasdsantos <99107296+camilasdsantos@users.noreply.github.com>
2022-11-29 13:01:29 -0300

    Add files via upload

---
+++ exemplo_sem_uso_de_funcao.js
@@ -0,0 +1,16 @@
+const dia1 = 31;
+const mes1 = 7;
+const ano1 = 2021;
+
+console.log(`Dia: ${dia1}`);
+console.log(`Mês: ${mes1}`);
+console.log(`Ano: ${ano1}`);
+
+
+const dia2 = 12;
+const mes2 = 6;
+const ano2 = 2022;
+
+console.log(`Dia: ${dia2}`);
+console.log(`Mês: ${mes2}`);
+console.log(`Ano: ${ano2}`);
