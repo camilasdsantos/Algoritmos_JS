3085b12447ed9982e2fe38aed07e7c6bd742730d
camilasdsantos <99107296+camilasdsantos@users.noreply.github.com>
2022-11-29 16:45:37 -0300

    Add files via upload

---
+++ exemplo_com_uso_funcao_02.js
@@ -0,0 +1,15 @@
+const dia1 = 31;
+const mes1 = 7;
+const ano1 = 2021;
+
+
+const dia2 = 12;
+const mes2 = 6;
+const ano2 = 2022;
+
+
+function exibirData(dia, mes, ano){
+console.log(`${dia}-${mes}-${ano}`);
+}
+exibirData(dia1, mes1, ano1);
+exibirData(dia2, mes2, ano2);
