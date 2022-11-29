c0502796ea3c3175b6ee0502ca0d8bb883373d4f
camilasdsantos <99107296+camilasdsantos@users.noreply.github.com>
2022-11-29 13:00:22 -0300

    Add files via upload

---
+++ break_continue.js
@@ -0,0 +1,14 @@
+//Break
+for(let i =1; i <= 5; i++){
+    console.log(i)
+    if (i === 3) break;
+}
+//continue
+for(let i =1; i <= 5; i++){
+    console.log(i)
+    if (i === 3) {
+        continue;
+}
+}
+// o break ele termina o laço for.
+// o continue não sai do laço, ele vai para a proxima repetição
