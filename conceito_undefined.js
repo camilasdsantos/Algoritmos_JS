15bf195c48db9cc7ffcd9894879537e2e9feceab
camilasdsantos <99107296+camilasdsantos@users.noreply.github.com>
2022-12-02 15:59:44 -0300

    Add files via upload

---
+++ conceito_undefined.js
@@ -0,0 +1,9 @@
+//variavel com valor não atribuido
+
+let a;
+let b=7;
+let c = null;//nill (algumas linguagens chama null de nill)
+let d = undefined;
+console.log(a,b,c,d);
+console.log(a+b);
+console.log(b+c);// b=7 , c = nulo, que nesse caso seria zero, entao b+c =7
