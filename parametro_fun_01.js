2537f805abad36abd5a9ff3ebb26b2ed78379e7b
camilasdsantos <99107296+camilasdsantos@users.noreply.github.com>
2022-12-08 17:25:15 -0300

    Add files via upload

---
+++ parametro_fun_01.js
@@ -0,0 +1,7 @@
+function somar(a,b,c,d){
+    return a +b +c+d;
+}
+console.log(somar(1,2,3,4));
+
+//Quando passmos para a função menos parametros do que ela pede gera um NAN (not a Number/undefined)
+console.log(somar(1,2,3,)); 
