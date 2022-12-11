ec9616b27574f0f6d33e106753d9472fdb093fe4
camilasdsantos <99107296+camilasdsantos@users.noreply.github.com>
2022-12-10 21:48:36 -0300

    Add files via upload

--- funcao_como_parametro_1.js
+++ funcao_como_parametro_1.js
@@ -4,6 +4,7 @@ function executar(fn){
     if (typeof executar === "function");
 }
 executar (3);
+
 console.log(typeof executar);
 // type of diz o tipo 
 console.log(typeof executar === "function");
