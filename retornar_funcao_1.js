a55031dad1b949546db3ba121d408ec5ba47674e
camilasdsantos <99107296+camilasdsantos@users.noreply.github.com>
2022-12-12 18:25:27 -0300

    Add files via upload

---
+++ retornar_funcao_1.js
@@ -0,0 +1,11 @@
+function retornar () {
+ function bomDia(){
+    return "bom dia!!!!!";
+ }
+ return bomDia;
+}
+//Em javaScript conseguimos criar uma função dentro de outra
+
+console.log(retornar);
+console.log(retornar());
+console.log(retornar()());
