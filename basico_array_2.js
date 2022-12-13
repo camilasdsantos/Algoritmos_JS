06d251436970fee6d6e0a4191b5955011dad355a
camilasdsantos <99107296+camilasdsantos@users.noreply.github.com>
2022-12-13 13:43:48 -0300

    Add files via upload

---
+++ basico_array_2.js
@@ -0,0 +1,13 @@
+//criando Array vazio e depois incluir os valores
+const nomes = [];
+console.log(typeof nomes);// tipo object
+nomes[0] = "Ana";
+nomes[1] = "Carla";
+nomes[2] = "Fernanda";
+nomes[3] = "Joana";
+nomes[4] = "Lizandra";
+nomes[10] = "Aline";
+
+
+//vai ser criado indice 10, mesmo sem os outros 5
+console.log(nomes);
