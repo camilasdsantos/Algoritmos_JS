639842d7b42643e7b676e42e24f88d14f90bb730
camilasdsantos <99107296+camilasdsantos@users.noreply.github.com>
2022-12-14 19:20:14 -0300

    Add files via upload

---
+++ basico_array3.js
@@ -0,0 +1,16 @@
+//method/metodo  é uma função que pertence a um objeto
+//Objeto
+
+console.log(typeof console);
+console.log(typeof console.log);
+
+const numeros = [1,2,3];
+
+console.log(typeof numeros);
+//console.(ponto é atributo)
+numeros.push(4);
+numeros.push(5);
+numeros.push(10.99);
+
+console.log(numeros);
+console.log(numeros.length);
