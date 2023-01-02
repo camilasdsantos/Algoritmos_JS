516fa78f834ff0b96120a5d005594d90d060982c
camilasdsantos <99107296+camilasdsantos@users.noreply.github.com>
2023-01-02 20:28:12 -0300

    Add files via upload

---
+++ metodos_3-5.js
@@ -0,0 +1,8 @@
+const numero =[10, 21, 333, 4, 5, 6, 86, 7, 8, 9, 10]
+
+function praCadaElemento(elemento, indice, array){
+    console.log(elemento, indice, array); // o primeiro parametro sempre será o elemento, e o segundo parametro sempre sera o indice, terceiro array completo
+
+}
+
+numero.forEach(praCadaElemento);
