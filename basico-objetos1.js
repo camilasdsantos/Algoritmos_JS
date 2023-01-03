c9b80a11cb16a887354051fcaf38f81b0b84e604
camilasdsantos <99107296+camilasdsantos@users.noreply.github.com>
2023-01-03 17:50:25 -0300

    Add files via upload

---
+++ basico-objetos1.js
@@ -0,0 +1,27 @@
+let x = 3;
+let y =[];
+
+const numeros = [11, 21, 31];
+
+//para verificar se é um array:
+
+console.log(Array.isArray(y));
+
+let a = {};
+console.log(typeof a);
+
+const produto = {
+    nome: 'caneta', preco: 9.99,
+    desconto: 0.32
+}
+
+console.log(numeros [0]);
+// PARA ACESSAR O INDICE DO PRODUTO, TAL COMO FAZEMOS COM ARRAYS 
+console.log(produto['nome'])
+//ouuu pelo ponto
+console.log(produto.nome);
+console.log(produto.preco);
+console.log(produto.desconto);
+
+console.log(typeof produto);
+
