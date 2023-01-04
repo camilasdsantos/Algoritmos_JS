a4320fd59035732f06003bbb7b6b7dab603f3fe3
camilasdsantos <99107296+camilasdsantos@users.noreply.github.com>
2023-01-04 18:31:37 -0300

    Add files via upload

---
+++ basico-objetos4.js
@@ -0,0 +1,9 @@
+const usuario ={
+    email: 'aluno@cod3r.com.br'
+
+};
+usuario.nome='aluno';
+usuario.senha = '12345678';
+console.log(usuario.email);
+console.log(usuario.nome);
+console.log(usuario);
